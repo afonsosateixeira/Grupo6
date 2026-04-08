0dc4a07cd3504df4cc82bfcc919358d17671154c
afonsosateixeira <afonsoSTPT@gmail.com>
2026-04-08 20:34:47 +0100

    fix: basedados

--- site/Extras/baseDados.sql
+++ site/Extras/baseDados.sql
@@ -1,24 +1,26 @@
-DROP SCHEMA IF EXISTS adoption;
-CREATE SCHEMA adoption;
-USE adoption;
+drop schema if exists adoption;
+create schema adoption;
+use adoption;
 
 drop table if exists species;
-CREATE TABLE species(
-    id INT NOT NULL auto_increment primary key,
-    name VARCHAR(20) NOT NULL
+create table species(
+    id INT NOT NULL auto_increment,
+    name VARCHAR(20) NOT NULL,
+    constraint pk_species_id primary key (id)
 ) engine=InnoDB;
 
 drop table if exists breeds;
-CREATE TABLE breeds(
-    id INT NOT NULL auto_increment primary key,
+create table breeds(
+    id INT NOT NULL auto_increment,
     specie_id INT NOT NULL,
     name VARCHAR(50) NOT NULL,
+    constraint pk_breeds_id primary key (id),
     constraint fk_species foreign key(specie_id) references species(id) on delete cascade
     ) engine= InnoDB;
 
 drop table if exists animals;
-CREATE TABLE animals(  
-    id INT NOT NULL auto_increment primary key,  
+create table animals(  
+    id INT NOT NULL auto_increment,  
     name VARCHAR(100) NOT NULL,
     breed_id INT NOT NULL,
     gender ENUM('M', 'F') NULL,
@@ -27,29 +29,32 @@ CREATE TABLE animals(
     description TEXT NULL,
     status ENUM('disponível', 'adotado', 'em processo') DEFAULT 'disponível',
     created_at TIMESTAMP default current_timestamp,
+    constraint pk_animals_id primary key (id),
     constraint fk_breeds foreign key(breed_id) references breeds(id) on delete cascade
     ) engine=InnoDB;
 
 
 drop table if exists adopters;
-CREATE TABLE adopters( 
-    id INT NOT NULL auto_increment primary key, 
+create table adopters( 
+    id INT NOT NULL auto_increment, 
     full_name VARCHAR(150) NOT NULL,  
     email VARCHAR(100) NOT NULL,  
     phone VARCHAR(20) NOT NULL,
     house_type ENUM('casa', 'apartamento') NULL,
-    status BOOLEAN DEFAULT TRUE, 
+    status BOOLEAN DEFAULT TRUE,
+    constraint pk_adopters_id primary key (id),
     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
     )engine=InnoDB;
 
 drop table if exists adoption_processes;
-CREATE table adoption_processes(
-    id INT NOT NULL auto_increment primary key,    
+create table adoption_processes(
+    id INT NOT NULL auto_increment,    
     adopter_id INT not null,    
     animal_id INT not null,    
     status ENUM('pendente', 'aprovado', 'rejeitado') default 'pendente',    
     start_date timestamp default current_timestamp,    
-    notes text null,    
+    notes text null,
+    constraint pk_adoption_processes_id primary key (id), 
     constraint fk_adopters foreign key(adopter_id) references adopters(id) on delete cascade,    
     constraint fk_animals foreign key(animal_id) references animals(id) on delete cascade
     ) engine=InnoDB;
